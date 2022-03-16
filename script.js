7263c660cd6d190175667957aaf1c2e89a9c2d15
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-03-16 20:21:04 +0100

    ..

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 
-// document.querySelector(".container").style.height = "1000px";
-
-// window.addEventListener("scroll", function() {
-//     var body = document.querySelector(".container");
-//     var height = body.style.height;
-//     // console.log(height);
-//     height = height.slice(0, -2);
-//     height = Number(height);
-//     return function() {
-//         if(height - window.scrollY < 600) {
-//             height += height / 2;
-//             console.log(height)
-//         }
-//         body.style.height = height + "px";
-//     };
-// }());
-
 const holder = document.querySelector('.container');
 const images = Array.from(document.querySelectorAll('.img-container'));
 // console.log(images);
@@ -23,7 +6,7 @@ const images = Array.from(document.querySelectorAll('.img-container'));
 window.addEventListener('scroll', ()=>{
     const {scrollHeight, scrollTop, clientHeight} = document.documentElement;
     if(scrollTop + clientHeight > scrollHeight - 5){
-        setTimeout(createPost);
+        createPost();
     }
 });
 
